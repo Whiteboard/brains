26c7c0dc3789c669bed61be9e2957dfa2f5d9055
Kody Dahl <kody@whiteboard.is>
2013-05-30 18:03:11 -0400

    jQuery things begin here

--- js/script.js
+++ js/script.js
@@ -8,12 +8,27 @@
 
 
 
+/*This bit runs the "We <ACTION> this thing" section in index.html*/
+//Hi. We're here to set some variables.
+    var expando = $(".expandographs"),
+        triggers = $(".header-triggers");
+    
+    //Event trigger
+    triggers.on("click", "h2", function(e){
+        // e refers to the browser event itself. Check out console.log(e) for more info.
+        var i = $(this).addClass("focus").index();
+        $(this).siblings().removeClass("focus");
+        expando.find(".expandograph").eq(i).addClass("open").siblings().removeClass("open");
+    });
 
-function homeNews(){
-        $('h2.light').click(function(){
-            if (!$(this).hasClass('open')) {
-                $('.expandograph.open').removeClass('open');
-                $(this).addClass('open');
-            }
-        });
-    }
+    //Arrow to cycle through actions
+    $(".forward.circle").on("click", function(){
+       // go to next section
+       var i = $(".expandograph.open").index();
+       if (i == $(".expandograph").length - 1) {
+           // we are at the last item
+           return false;
+       } else {
+            triggers.find("h2").eq(i+1).trigger("click");
+       }
+    });
