c8656737dde9acddec77e2ed40d4da2496ebbf25
Kody Dahl <kody@whiteboard.is>
2013-05-31 15:59:11 -0400

    Slider + Category links on home page

--- js/script.js
+++ js/script.js
@@ -32,3 +32,72 @@
             triggers.find("h2").eq(i+1).trigger("click");
        }
     });
+
+
+    
+
+/*This bit is the slider on the homepage*/
+    var delay = 4000, // delay in ms
+        tO; // timeout
+    
+    function startSlider(){
+        clearTimeout(tO);
+        tO = setTimeout(goToSlide, delay);
+    }
+    
+    function setupControls(){
+        
+        $(".slide-controls").on("click", ".slide-control", function(e){
+            
+            // e is the event. If these are links, do the next line.
+            e.preventDefault();
+            
+            var theIndex = $(this).index();
+            
+            // You may want to do this as well, although you could do this in the actual goToSlide function for portability.
+            // $(this).addClass("current").siblings().removeClass("current");
+
+            goToSlide(theIndex);
+        });
+        
+    }
+    
+    function goToSlide(i){
+        var allslides = $(".slide"),
+            currslide = allslides.filter(".current");        
+        if (i){
+            // if you pass in an i to goToSlide(), go to that slide. Useful for slide button controls.
+            var nextslide = allslides.eq(i);
+        } else {
+            // let's go to the next slide by default
+            var nextslideIndex = (function(){
+            
+                if (currslide.index() == allslides.length -1){
+                    // we are at the last slide, so let's loop back around
+                    return 0;
+                } else {
+                    return currslide.index()+1;
+                }
+            
+            }()); // this is called a self invoking anonymous function (or siaf for a really hard acronym to remember)
+            var nextslide = allslides.eq(nextslideIndex);
+        }
+        
+        // now, go.
+        $(".slider-control").eq(nextslideIndex).addClass("current").siblings().removeClass("current");
+        nextslide.addClass("current").siblings().removeClass("current");
+
+        animateSlides(nextslide);
+        startSlider();
+    }
+    function animateSlides(toslide){
+        // toslide is the slide we are animating to... of course
+        var strip = $(".slidewrap-inner");
+        // here is where you could do some Modernizr magic to see if CSS transitions are available, but eff that for now.
+        strip.css({
+            marginLeft : toslide.index() * -100 + "%"
+        });
+    }
+
+    // let's kick this pig
+    startSlider();
